4d4bf5958d0f640bbad0c22caac6e63b76294a4c
kosiv1989-88 <kosiv1989@gmail.com>
2023-10-24 15:17:58 +0800

    +script2 уч

--- script.js
+++ script.js
@@ -21,37 +21,3 @@ personaMovieDB.movies[c] = d;
 
 console.log(personaMovieDB);
 
-// let hamburger;
-// const fries = NaN;
-// const cols = 0;
-// const nuggets = 2;
-
-// if (hamburger && cols || fries ===3 && nuggets) {
-//     console.log("Done")
-// } 
-
-
-
-// for (let i = 0; i < 3; i++) {
-//     console.log(i)
-//     for (let i = 0; i < 3; i++) {
-//         console.log(i)    
-//     }
-// }
-
-
-
-// let result = '';
-// const lenght = 7;
-
-// for (let i = 1; i < lenght; i++) {
-
-//     for (let j = 0; j < i; j++) {
-//         result += '*';
-//     }
-
-
-//     result += '\n';
-// }
-
-// console.log(result);
---
+++ script2.js
@@ -0,0 +1,115 @@
+// let hamburger;
+// const fries = NaN;
+// const cols = 0;
+// const nuggets = 2;
+
+// if (hamburger && cols || fries ===3 && nuggets) {
+//     console.log("Done")
+// } 
+
+
+
+// for (let i = 0; i < 3; i++) {
+//     console.log(i)
+//     for (let i = 0; i < 3; i++) {
+//         console.log(i)    
+//     }
+// }
+
+
+
+// let result = '';
+// const lenght = 7;
+
+// for (let i = 1; i < lenght; i++) {
+
+//     for (let j = 1; j < i; j++) {
+//         result += '*';
+//     }
+
+
+
+//     result += '\n';
+// }
+
+// console.log(result);
+
+// for (let i = 20; i > 9; i--) {
+//     console.log(i);
+//     if (i === 13) break
+// }
+
+// for (let i = 2; i < 11; i++) {
+//     if (i %2=== 0) {
+//         console.log(i)
+//     }
+// }
+
+// for (let i = 2; i <= 16; i++) 
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+
+// let num = 2;
+
+// while(num <= 15) {
+//     num++;
+//     if (num %2===0) continue;
+
+//     else 
+//         console.log(num)
+// }
+
+// const arr = [];
+
+// for (i = 5; i < 11; i++) {
+//     arr[i - 5] = i;
+    
+// }
+// console.log(arr);
+
+// const arrayOfNumbers = [];
+
+//     for (let i = 5; i < 11; i++) {
+//         arrayOfNumbers[i - 5] = i;
+//     }
+
+//     console.log(arrayOfNumbers);
+
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+// const result = [];
+ 
+// for(i = 0; i < arr.length; i++) {
+//     result[i] = arr[i];
+// }
+// console.log(result);
+
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+//     for (i = 0; i < data.length; i++) {
+//         if (typeof(data[i]) === 'number' ) {
+//             data[i] = data[i] * 2;
+//         }
+//          else if (typeof(data[i]) === 'string') {
+//                 data[i] = `${data[i]} - done`; 
+//             }   
+//     }
+//     console.log(data)
+
+    // const data = [5, 10, 'Shopping', 20, 'Homework'];
+    // const result = [];
+
+    // for (i = 1; i <= data.length; i++) {
+    //     result[i - 1] = data[data.length - i]
+    // }
+    // console.log(result)
+
+
+    const lines = 5;
+    let result = '';
+    
+    for (let i =0; i < lines; i++) {
+    
+    }
+    
